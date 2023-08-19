4a92cd7911f1337161da9c5d217f4e129203278b
eoeo <eoeo@localhost.localdomain>
2023-08-19 21:53:59 +0300

    fix port var

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.delete("/api/notes/:id",(request,response)=>{
 //     response.end(JSON.stringify(notes))
 // })
 
-const PORT = process.env || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`Server listening in por ${PORT}`)
 })
