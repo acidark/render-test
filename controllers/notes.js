a22b7ca6999e7a0f1782eb79cc20832e6b55da36
eoeo <eoeo@localhost.localdomain>
2023-10-10 23:00:23 +0300

    fixed token string

--- controllers/notes.js
+++ controllers/notes.js
@@ -9,57 +9,26 @@ notesRouter.get('/',async (request,response) => {
     .populate('user',{ username:1,name:1 })
   response.json(notes)
 })
-
-// notesRouter.get('/:id',(request,response,next) => {
-//   Note
-//     .findById(request.params.id)
-//     .then(note => {
-//       if(note){
-//         response.json(note)
-//       } else {
-//         response.status(404).end()
-//       }
-//     }).catch(error => next(error))
-// })
-
 notesRouter.get('/:id', async(request,response) => {
-  // try {
   const note = await Note.findById(request.params.id)
   if(note) {
     response.json(note)
   } else {
     response.status(404).end()
   }
-  // } catch (exception) {
-  // next(exception)
-  // }
-})
 
-// notesRouter.post('/',(request,response,next) => {
-//   const { content,important } = request.body
-//   const note = new Note ({
-//     content:content,
-//     important:important||false
-//   })
-//   note
-//     .save()
-//     .then(savedNote => {
-//       response.status(201).json(savedNote)
-//     }).catch(error => next(error))
-// })
+})
 const getTokenFrom = request => {
   let authorization = request.get('authorization')
-  if(authorization && authorization.starsWith('Bearer')) {
-    return authorization = authorization.replace('Bearer','')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization = authorization.replace('Bearer ','')
   }
   return null
 }
 notesRouter.post('/',async(request,response) => {
   const decodedToken = jwt.verify(getTokenFrom(request),process.env.SECRET)
-  if(!(decodedToken.id)) {
-    return response.status(401).json({
-      error:'invalid token'
-    })
+  if(!decodedToken.id) {
+    return response.status(401).json({ error:'invalid token' })
   }
   const body = request.body
   const user = await User.findById(decodedToken.id)
@@ -74,26 +43,10 @@ notesRouter.post('/',async(request,response) => {
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
   response.status(201).json(savedNote)
-  // } catch(exception) {
-  // next(exception)
-  // }
 })
-
-// notesRouter.delete('/:id',(request,response,next) => {
-//   Note
-//     .findByIdAndRemove(request.params.id)
-//     .then(() => {
-//       response.status(204).end()
-//     }).catch(error => next(error))
-// })
-
 notesRouter.delete('/:id', async (request,response) => {
-  // try {
   await Note.findByIdAndRemove(request.params.id)
   response.status(204).end()
-  // } catch (exception) {
-  // next(exception)
-  // }
 })
 
 notesRouter.put('/:id',(request,response,next) => {
